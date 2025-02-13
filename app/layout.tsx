5c52e9b86672419405509efd94c228c04938f652
asma <bahriniasma453@gmail.com>
2025-02-13 11:18:42 +0100

    fixing some build errors before deploying to vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { DM_Sans } from "next/font/google";
 import clsx from "clsx";
 
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
